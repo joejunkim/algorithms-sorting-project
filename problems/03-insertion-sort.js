9dd77c45dea434a0891ee2aaa64b259c4c70277d
Joubin T <jubin.taghdir@gmail.com>
2021-04-20 16:01:11 -0700

    merge

--- problems/03-insertion-sort.js
+++ problems/03-insertion-sort.js
@@ -1,8 +1,37 @@
 // Implement Insertion Sort
+function swap(list, index1, index2){
+  let temp = list[index1];
+  list[index1] = list[index2];
+  list[index2] = temp;
+}
+
 
 function insertionSort(list) {
   // your code here
+
+  let min = 100;
+  
+  for(let i = 1; i < list.length; i++){
+
+    if(list[i + 1] < list[i]){
+
+      for(let j = i; j < list.length; j++){
+
+        if(list[i] < list[j]){
+
+          swap(list, i, j)
+
+        }
+
+      }
+      
+    }
+
+  }
 }
+
+let array = [ 2, -1, 3, 4, 3, 7];
+insertionSort(array);
 
 module.exports = {
   insertionSort
--- problems/04-merge-sort.js
+++ problems/04-merge-sort.js
@@ -1,12 +1,45 @@
 // Implement Merge Sort
 
 function merge(array1, array2) {
-  // your code here
+let mergedArr = [];
+
+  while(array1.length || array2.length){
+
+    if(array1[0] < array2[0]){
+  
+      let holder = array1.shift();
+      mergedArr.push(holder);
+  
+    } else {
+  
+      let holder = array2.shift();
+      mergedArr.push(holder);
+
+    }
+  }
+
+  return mergedArr.concat(array1).concat(array2);
+  
 }
 
 function mergeSort(array) {
-  // your code here
+  
+  if((array.length === 1) || (array.length === 0)){
+    return array;
+  }
+
+  let middle = Math.floor(array.length / 2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle);
+
+
+  let newLeft = mergeSort(left);
+  let newRight = mergeSort(right);
+
+  return merge(newLeft, newRight);
 }
+
+mergeSort([2, -1, 4, 3, 7, 3])
 
 module.exports = {
   merge,
