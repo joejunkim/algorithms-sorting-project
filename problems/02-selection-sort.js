79ac75becd1f71a5442239ba73f8c4c2a42add86
Joubin T <jubin.taghdir@gmail.com>
2021-04-20 10:53:35 -0700

    phase 2 incomplete

--- problems/02-selection-sort.js
+++ problems/02-selection-sort.js
@@ -1,12 +1,36 @@
 // Implement Selection Sort
+/*
 
+  - set min placeholder
+    - Search for smallest value(S)
+      - if S < placeHoleder => swap
+      - else placeHolder ++
+  - repeat until placeholder = length
+
+*/
 // Implement swap without looking at bubble sort
 function swap(arr, index1, index2) {
-  // your code here
+  let temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
 }
 
 function selectionSort(list) {
-  // your code here
+  let placeHolder = 0;
+  let smallest;
+
+  while(placeHolder < arr.length - 1){
+    if(arr[smallest] < arr[placeHolder]){
+
+      swap(list, placeHolder, smallest);
+
+    } else {
+
+      placeHolder++;
+
+    }
+    smallest++;
+  }
 }
 
 module.exports = {
